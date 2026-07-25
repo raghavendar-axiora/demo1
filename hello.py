8d51aec906c7d891bb58375477b19e955ee5d845
raghavendar-axiora <raghavendar.y@axioraglobalsolutions.com>
2026-07-25 11:50:15 +0530

    updated the docker file

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
+import time
+
 def main():
-    print("Hello from my Dockerized Python app!")
+    while True:
+        print("Hello from my Dockerized Python app!")
+        time.sleep(5)
 
 if __name__ == "__main__":
-    main()    
+    main()
